269b8376ed30a5c8937dfa903d119283b059dea5
HusainBallam <hballamco@gmail.com>
2025-03-26 11:26:31 +0300

    second branch created with new changes

--- tutorial_script.py
+++ tutorial_script.py
@@ -5,3 +5,7 @@ print("Hello World...")
 # This comment was generated to test git branches
 
 print("A new branch was created")
+
+# This comment was generated to test git branches 2
+
+print("Another branch was created")
